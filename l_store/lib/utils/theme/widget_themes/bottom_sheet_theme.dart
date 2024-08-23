8d343a316c5194def4bfb830b08027ea314092e0
Michael Henry <mdhenry763@gmail.com>
2024-08-23 20:28:45 +0200

    chore(themes): adding themes.

---
+++ l_store/lib/utils/theme/widget_themes/bottom_sheet_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:l_store/utils/constants/colors.dart';
+
+class LBottomSheetTheme {
+  LBottomSheetTheme._();
+
+  static BottomSheetThemeData lightBottomSheetTheme = BottomSheetThemeData(
+    showDragHandle: true,
+    backgroundColor: TColors.white,
+    modalBackgroundColor: TColors.white,
+    constraints: const BoxConstraints(minWidth: double.infinity),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+  );
+
+  static BottomSheetThemeData darkBottomSheetTheme = BottomSheetThemeData(
+    showDragHandle: true,
+    backgroundColor: TColors.black,
+    modalBackgroundColor: TColors.black,
+    constraints: const BoxConstraints(minWidth: double.infinity),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+  );
+}
